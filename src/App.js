35ebf3da183ac2795209079549abc4b26566f750
Samar Omar <samaromm98@gmail.com>
2020-03-05 22:28:13 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -4,6 +4,6 @@ import React, {Component} from 'react';
 export default class App extends Component{
   componentDidMount(){
     fetch("http://api.open-notify.org/astros.json")
-      .then()
+      .then(res=>res.json)
   }
 }
